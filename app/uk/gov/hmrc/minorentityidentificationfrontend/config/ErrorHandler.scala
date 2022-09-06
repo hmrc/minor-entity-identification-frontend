2322eaa08c0325db89fba8ca906e8549afd73b22
woollett-r <55831974+woollett-r@users.noreply.github.com>
2022-09-06 12:12:32 +0100

    Example error handling of custom journey exception

--- app/uk/gov/hmrc/minorentityidentificationfrontend/config/ErrorHandler.scala
+++ app/uk/gov/hmrc/minorentityidentificationfrontend/config/ErrorHandler.scala
@@ -16,27 +16,38 @@
 
 package uk.gov.hmrc.minorentityidentificationfrontend.config
 
+import play.api.http.HeaderNames.CACHE_CONTROL
 import play.api.{Configuration, Environment, Logging}
 
 import javax.inject.{Inject, Singleton}
-import play.api.i18n.MessagesApi
-import play.api.mvc.Results.NotFound
+import play.api.i18n.{Messages, MessagesApi}
+import play.api.mvc.Results.{InternalServerError, NotFound}
 import play.api.mvc.{Request, RequestHeader, Result}
 import play.twirl.api.Html
 import uk.gov.hmrc.auth.core.AuthorisationException
 import uk.gov.hmrc.http.NotFoundException
-import uk.gov.hmrc.minorentityidentificationfrontend.views.html.templates.error_template
+import uk.gov.hmrc.minorentityidentificationfrontend.errors.JourneyException
+import uk.gov.hmrc.minorentityidentificationfrontend.models.JourneyConfig
+import uk.gov.hmrc.minorentityidentificationfrontend.services.JourneyService
+import uk.gov.hmrc.minorentityidentificationfrontend.utils.MessagesHelper
+import uk.gov.hmrc.minorentityidentificationfrontend.views.html.templates.{error_template, journey_error_template}
 import uk.gov.hmrc.play.bootstrap.frontend.http.FrontendErrorHandler
 import uk.gov.hmrc.play.bootstrap.config.AuthRedirects
 
 import scala.concurrent.Future
 
+import scala.concurrent.ExecutionContext
+
 @Singleton
 class ErrorHandler @Inject()(val messagesApi: MessagesApi,
+                             val messagesHelper: MessagesHelper,
+                             val journeyService: JourneyService,
                              view: error_template,
+                             journeyView: journey_error_template,
                              val config: Configuration,
                              val env: Environment
-                            )(implicit val appConfig: AppConfig) extends FrontendErrorHandler with AuthRedirects with Logging  {
+                            )(implicit val appConfig: AppConfig,
+                              executionContext: ExecutionContext) extends FrontendErrorHandler with AuthRedirects with Logging  {
 
   override def standardErrorTemplate(pageTitle: String,
                                      heading: String,
@@ -48,6 +59,7 @@ class ErrorHandler @Inject()(val messagesApi: MessagesApi,
   override def onServerError(request: RequestHeader, exception: Throwable): Future[Result] = {
     exception match {
       case _: AuthorisationException => Future.successful(resolveError(request, exception))
+      case jEx : JourneyException => resolveJourneyException(request, jEx)
       case _ => super.onServerError(request, exception)
     }
   }
@@ -63,4 +75,40 @@ class ErrorHandler @Inject()(val messagesApi: MessagesApi,
         super.resolveError(rh, ex)
     }
   }
+
+  private def resolveJourneyException(rh: RequestHeader, journeyException: JourneyException): Future[Result] = {
+
+    logException(rh, journeyException)
+
+    journeyService.getJourneyConfig(journeyException.journeyId, journeyException.authInternalId).map {
+      journeyConfig =>
+      InternalServerError(journeyErrorTemplate(journeyConfig)(Request(rh, ""))).withHeaders(CACHE_CONTROL -> "no-cache")
+    } recover {
+      case ex: Exception => super.resolveError(rh, ex)
+    }
+
+  }
+
+  private def journeyErrorTemplate(journeyConfig: JourneyConfig)(implicit request: Request[_]): Html = {
+
+    val messages: Messages = messagesHelper.getRemoteMessagesApi(journeyConfig).preferred(request)
+
+    journeyView(
+      "global.error.InternalServerError500.title",
+      "global.error.InternalServerError500.heading",
+      "global.error.InternalServerError500.message",
+      journeyConfig.pageConfig
+    )(request, messages, appConfig)
+
+  }
+
+  private def logException(request: RequestHeader, ex: Throwable): Unit =
+    logger.error(
+      """
+        |
+        |! Journey exception, for (%s) [%s] ->
+        | """.stripMargin.format(request.method, request.uri),
+      ex
+    )
+
 }
